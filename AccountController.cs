0388cb48ab7e0241772dcfdfa1e22d29e126b5c1
Konrad Stefaniak <68376449+stefaniak-konrad@users.noreply.github.com>
2020-07-16 12:34:58 +0200

    Add files via upload

---
+++ AccountController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using EO.Serwis.Portal.DataAccess.Contract.POCO;
+using EO.Serwis.Portal.DataAccess.Contract.Repositories;
+using EO.Serwis.Portal.WebApi.Core3.Helpers;
+using EO.Serwis.Portal.WebApi.Core3.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace EO.Serwis.Portal.WebApi.COre3.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public IParametrySystemuRepository ParametrySystemuRepository { get; }
+        public IUzytkownicyRepository UzytkownicyRepository { get; }
+        public IEmailRepository EmailRepository { get; }
+        public IConfiguration Conf { get; }
+
+        public AccountController(IParametrySystemuRepository parametrySystemuRepository, IUzytkownicyRepository uzytkownicyRepository, IEmailRepository emailRepository, IConfiguration conf)
+        {
+            ParametrySystemuRepository = parametrySystemuRepository;
+            UzytkownicyRepository = uzytkownicyRepository;
+            EmailRepository = emailRepository;
+            Conf = conf;
+        }
+
+        [HttpPost]
+        [ActionName("InitializePasswordChange")]
+        public IActionResult InitializePasswordChange([FromBody] dynamic model)
+        {
+            try
+            {
+                Log.Information($"Getting user by login: {model?.Login}.");
+                var url = Conf.GetSection("UserSecrets")["Url"];
+                var login = (string)model.Login;
+                var user = UzytkownicyRepository.Select(p => p.Login == login, c => c.SesjeUzytkownika).SingleOrDefault();
+                if (user == null)
+                {
+                    Log.Error("Uzytkownik o podanym loginie nie został odnaleziony w bazie.");
+                    return BadRequest("Uzytkownik o podanym loginie nie został odnaleziony w bazie.");//jeśli nie istnieje to error               
+                }
+                var guid = Guid.NewGuid().ToString();
+                var sessionId = guid.Substring(0, 5) + guid.Substring(28, 5);
+                var parametry = ParametrySystemuRepository.Select(p => p.KategoriaParametru == "Email" && p.KategoriaIIParmetru == "PortalWyceny_ZmianaHasla").ToList();
+
+                if (user.SesjeUzytkownika.Count > 0)
+                {
+                    user.SesjeUzytkownika.RemoveAll(p => p.UzytkownikPortaluId == user.Id);
+                }
+                user.SesjeUzytkownika.Add(new UzytkownicyPortaluSesjePOCO()
+                {
+                    DataWaznosciSesjiZmianyHasla = DateTime.Now.AddDays(1),
+                    Id = sessionId
+                });
+
+                UzytkownicyRepository.Modify(user);
+                UzytkownicyRepository.Save();
+
+                Log.Information($"Dodano nową sesję zmiany hasła dla loginu : {model?.Login}.");
+
+                EmailRepository.Add(new EmailPOCO()
+                {
+                    Status = "Oczekuje na wysłanie",
+                    Temat = parametry.Single(p => p.NazwaParametru == "EmailTemat").WartoscParametru.Replace("<UserPortal>", $"{user.Imie} {user.Nazwisko}"),
+                    OdAddress = "serwis@eo.pl",
+                    OdDisplayName = "Serwis EO Networks",
+                    DoAddress = ((string)model.Login).Trim(),
+                    DoDisplayName = $"{user.Imie} {user.Nazwisko}",
+                    MessageiD = Guid.NewGuid().ToString(),
+                    Header_Thread_Index = Guid.NewGuid().ToString(),
+                    Priorytet = 0,
+                    EmailPrzyjety = false,
+                    Format = "html",
+                    Wiadomosc = parametry.Single(p => p.NazwaParametru == "EmailTresc").WartoscParametru.Replace("<Link>", $"{url}/account/changepassword/{sessionId}").Replace("<UserPortal>", $"{user.Imie} {user.Nazwisko}"),
+                    DataWyslania = DateTime.Now,
+                    DataOdbioru = DateTime.Now,
+                    WiadomoscPrzetworzona = 0
+                });
+                EmailRepository.Save();
+
+                Log.Information($"Pod adress {model?.Login} został wysłany email z linkiem do zmiany hasła.");
+
+                return Ok();
+            }
+
+            catch (Exception ex)
+            {
+                Log.Fatal(ex.ToString());
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        [ActionName("ChangePassword")]
+        public IActionResult ChangePassword([FromBody] dynamic model)
+        {
+            try
+            {
+                Log.Information($"Changing password for session: {model?.SessionId}.");
+                var sessionId = (string)model.SessionId;
+                var user = UzytkownicyRepository.Select(p => p.SesjeUzytkownika.Select(d => d.Id).Contains(sessionId), c => c.SesjeUzytkownika).SingleOrDefault();
+                if (user == null)
+                {
+                    Log.Error($"Nie odnaleziono sesji o identyfikatorze {(string)model.SessionId} w bazie danych.");
+                    return BadRequest($"Nie odnaleziono sesji o identyfikatorze {(string)model.SessionId} w bazie danych.");//jeśli nie istnieje to error               
+                }
+
+                user.Password = HashHelper.CalculateSHA2Hash(user.RowUID + model?.Password).ToLower();
+                UzytkownicyRepository.Modify(user);
+                UzytkownicyRepository.Save();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex.ToString());
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ ZgloszeniaController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EO.Serwis.Portal.DataAccess.Contract.Repositories;
+using EO.Serwis.Portal.DataAccess.Repositories;
+using EO.Serwis.Portal.WebApi.Core3.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace EO.Serwis.Portal.WebApi.Core3.Controllers
+{
+    //[Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ZgloszeniaController : ControllerBase
+    {
+        public IWycenyRepository WycenyRepo { get; }
+        public IZgloszenieRepository ZgloszeniaRepo { get; }
+
+        public ZgloszeniaController(IWycenyRepository wycenyRepo, IZgloszenieRepository zgloszeniaRepo)
+        {
+            WycenyRepo = wycenyRepo;
+            ZgloszeniaRepo = zgloszeniaRepo;
+        }
+
+        [HttpGet]
+        [Route("api/Zgloszenia")]
+        public IActionResult Get([FromQuery] long idUzytkownika)
+        {
+            var list = WycenyRepo.GetWyceny(idUzytkownika);
+            return Ok(list.Select(c =>
+                new
+                {
+                    IdZgloszenia = c.IdZgloszenia,
+                    IdWyceny = c.IdWyceny,
+                    Opis = c.Zgloszenie.OpisSzczeglowy,
+                    UrlWyceny = c.UrlWyceny
+                }
+                ).ToList());
+        }
+
+        [HttpGet]
+        [Route("api/Zgloszenia/DaneUzytkownika")]
+        public IActionResult GetCustomerData([FromQuery] long idZgloszenia)
+        {
+            try
+            {
+                var zgloszenie = ZgloszeniaRepo.FindEntity(idZgloszenia);
+
+                //var wycena = WycenyRepo.Select(p => p.IdWyceny == zgloszenie.);
+
+                if (zgloszenie == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(new CustomerDataModel()
+                {
+                    IdZgloszenia = zgloszenie.IdZgloszenia,
+                    Imie = zgloszenie.UzytkownikImie,
+                    Nazwisko = zgloszenie.UyztkownikNazwisko,
+                    Adres = zgloszenie.UzytkownikAdres,
+                    KodPocztowy = zgloszenie.UzytkownikKodPocztowy,
+                    Miasto = zgloszenie.UzytkownikMiasto,
+                    Mail = zgloszenie.UzytkownikEmail,
+                    TelefonKomorkowy = zgloszenie.UzytkownikTelefonKomorkowy,
+                    WyborRealizatora = zgloszenie.WyborRealizatora
+                });
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex.ToString());
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost]
+        [Route("api/zgloszenia/update")]
+        public IActionResult UpdateServiceOrder([FromBody] CustomerDataModel model)
+        {
+            var zgloszenie = ZgloszeniaRepo.FindEntity(model.IdZgloszenia);
+
+            if (zgloszenie == null)
+            {
+                return NotFound();
+            }
+            {
+                zgloszenie.IdZgloszenia = model.IdZgloszenia;
+                zgloszenie.UzytkownikImie = model.Imie;
+                zgloszenie.UyztkownikNazwisko = model.Nazwisko;
+                zgloszenie.UzytkownikMiasto = model.Miasto;
+                zgloszenie.UzytkownikAdres = model.Adres;
+                zgloszenie.UzytkownikKodPocztowy = model.KodPocztowy;
+                zgloszenie.UzytkownikEmail = model.Mail;
+                zgloszenie.UzytkownikTelefonKomorkowy = model.TelefonKomorkowy;
+            }
+
+            ZgloszeniaRepo.Modify(zgloszenie);
+            ZgloszeniaRepo.Save();
+
+            return Ok();
+        }
+
+    }
+}
